a37ccf3f2f9298edf7118f9dcd693aaa44929b2e
Estudiante <sanmhepn@hotmail.com>
2019-04-16 19:18:25 -0500

    Ejersicio 2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,15 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+
+echo "<center>";
+$bloque= "<h1> Fernando Sanmartin </h1>";
+
+for($i=0; $i<=2; $i++){
+
+	echo "$bloque";
+	echo "<br/>";
+}
+
+
 ?>
